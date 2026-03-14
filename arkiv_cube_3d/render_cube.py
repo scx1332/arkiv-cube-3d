031794d8925f825493d75fb90203915448caa6a0
Sieciech Czajka <sieciech@gmail.com>
2026-03-14 22:26:17 +0100

    perfect camera height

--- arkiv_cube_3d/render_cube.py
+++ arkiv_cube_3d/render_cube.py
@@ -308,7 +308,7 @@ def setup_camera():
     # --- Camera ---
     cam_data = blender.data.cameras.new("Camera")
     camera = blender.data.objects.new("Camera", object_data=cam_data)
-    camera.location = (0, -0.01, 30)
+    camera.location = (0, -0.01, 26)
     scene_collection.objects.link(camera)
 
     # --- Constraints ---
